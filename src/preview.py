f82fdf142b9fbfeb2b8359a8261742e75fab1d03
Travis Geis <ottobonn28@gmail.com>
2017-05-09 15:27:56 -0700

    Add trajectory preview script

---
+++ src/preview.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python2
+
+import json
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main(filename):
+    with open(filename) as jsonfile:
+        trajectory = json.load(jsonfile)
+        plt.figure()
+        for toolpath in trajectory["sequence"]:
+            points = np.array(toolpath["points"])
+            plt.plot(points[:,0], points[:,1], "o")
+        plt.show()
+    return 0
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        sys.exit(main(sys.argv[1]))
+    else:
+        print "Usage: ./preview.py <json filename>"
