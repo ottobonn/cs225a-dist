e2fdd49d620e1c00f7a522ed89da6ad5581cc56d
WenYuanlin <Yuanlin@DN51s4gj.SUNet>
2017-05-24 00:10:48 -0700

    color.py
    yuanlin

--- src/color.py
+++ src/color.py
@@ -3,112 +3,6 @@ import numpy as np
 import json
 import sys
 
-MAX = 0
-
-def calculateDist(point0,point1):
-
-    return np.linalg.norm(point0-point1)
-
-## Inputs:
-#   Nodes: a vector of [start_point end_point] of all lines in found contour
-#   loc: current position
-#   side: begin side( 0 ) or end side( 1 )
-def bestRouteHelper(nodes, availIndices, currIndex, currSubIndex, route, cumDist):
-    n = len(availIndices)
-    #Base Case: nodes is empty (all nodes added to route)
-    if n == 0:
-        return cumDist
-    minDist         = MAX
-    minNextIndexOfIndex    = None
-    minNextSubIndex = None
-
-    #find next closest node
-    for i in range(n):
-        nextIndex = availIndices[i]
-        for nextSubIndex in (0,1):
-            d = calculateDist(nodes[currIndex][1-currSubIndex], nodes[nextIndex][nextSubIndex])
-            if d < minDist:
-                minDist = d
-                minNextIndexOfIndex = i
-                minNextSubIndex = nextSubIndex
-
-
-
-    minNextIndex = availIndices.pop(minNextIndexOfIndex)
-    route.append((minNextIndex,minNextSubIndex))
-    cumDist += minDist
-    finalDist = bestRouteHelper(nodes, availIndices, minNextIndex, minNextSubIndex, route, cumDist)
-    availIndices.insert(minNextIndexOfIndex, minNextIndex)
-    return finalDist
-
-## Input:
-# contours: contour output from findContour.
-#
-# Determines the best order for the robot to traverse through the contours
-# in order to minimize distance travelled, and outputs the new contour
-# Modifications to contour includes:
-# - re-ordering the contours
-# - reversing a contour
-def bestRoute(contours):
-    nodes = []
-    n = len(contours)
-    min = MAX
-    route = None
-    indices = list(range(n))
-    for i in range(n):
-        nodes.append((contours[i][0][0],contours[i][-1][0]))
-
-    for i in range(n):
-        currIndex=indices.pop(i)
-        for currSubIndex in (0,1):
-            routeSoFar = [(currIndex, currSubIndex)]
-            d = bestRouteHelper(nodes, indices, currIndex, currSubIndex, routeSoFar, 0)
-            if d<min:
-                min = d
-                route = routeSoFar
-        indices.insert(i,currIndex)
-
-    newContours = []
-    for i in range(n):
-        index,subIndex = route[i]
-        if subIndex == 0:
-            newContours.append( contours[i] )
-        else:
-            newContours.append( contours[i][::-1] )
-    #print(newContours)
-    return newContours
-
-## Inputs:
-# image: one color layer of the image. Data type: Matrix
-# image_width: width of the image
-# image_height: height of the image
-#
-# This function parses the color layer into vertical lines of consequtive points.
-# Then calls bestRoute to return the planned path for drawing
-
-def colorPath(image,image_width,image_height):
-    path = []
-    line = []
-    for col in range(image_width):
-        start = 0
-        for row in range(image_height): # straight lines from up to down
-            if image[row][col] == 1: # start and inside the line
-                start = 1
-                line.append([row,col])
-            elif image[row][col] == 0 and start == 1: # end of line
-                # only draw lines that are >=3 pixels. avoiding sharp shapes.
-                if len(line)>=3:
-                    path.append(line)
-                
-                line = []
-                start = 0
-
-    # Call same path planning function as contour, since already parsed shape in to lines
-    return bestRoute(path)
-
-
-
-
 def main(input_image, output_contour_dir, output_json_dir):
     im = cv2.imread(input_image)
     resize_width = 380
@@ -139,6 +33,9 @@ def main(input_image, output_contour_dir, output_json_dir):
     sequences = []
     matrixes = []
     
+    preview_img = np.zeros((resize_width, resize_height, 3))
+
+
     for i in range(len(colors)):
         JSONs.append({})
         JSONs[i]['version'] = '1.0.0'
@@ -157,19 +54,30 @@ def main(input_image, output_contour_dir, output_json_dir):
                     match_c = k
                     dist = dist_k
             matrixes[match_c][i][j] = 1
-    
-    for c in range(len(colors)):
-        paths = colorPath(matrixes[c], resize_width, resize_height)
-        for l in paths:
-            cell = {}
-            cell['tool'] = c
-            cell['points'] = l/float(max_pos)
-            sequence.append(cell)
-            JSONs[i]['sequence'] = sequence
 
-        with open(output_json_dir+str(c)+'.json', 'w') as outfile:
-            json.dump(JSONs[c], outfile)
+            preview_img[i, j, :] = colors[match_c]
+    
+    # for c in range(len(colors)):
+    #     paths = colorPath(matrixes[c], resize_width, resize_height)
+    #     for l in paths:
+    #         cell = {}
+    #         cell['tool'] = c
+    #         cell['points'] = l/float(max_pos)
+    #         sequence.append(cell)
+    #         JSONs[i]['sequence'] = sequence
+
+    #     with open(output_json_dir+str(c)+'.json', 'w') as outfile:
+    #         json.dump(JSONs[c], outfile)
+
     ######## end output JSON #####################
+
+
+    ######## preview
+
+    cv2.imshow('preview', preview_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
 
     ### save the contours to a picture
     #output = np.zeros(im.shape, np.uint8)
@@ -186,7 +94,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 4:
         sys.exit(main(sys.argv[1], sys.argv[2], sys.argv[3]))
     else:
-        print "Usage: ./edge.py <input_image> <output_contour_image> <output_json_file>"
+        print "Usage: ./color.py <input_image> <output_contour_image> <output_json_file>"
 
 
 
